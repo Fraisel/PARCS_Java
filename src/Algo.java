fd4303980258fab2817e2d2f1bfd99b7c4b412d3
Denys Kochetkov <deniskochetkov1@gmail.com>
2021-04-08 02:39:58 +0200

    Update Algo.java

--- src/Algo.java
+++ src/Algo.java
@@ -5,21 +5,21 @@ import parcs.*;
 
 public class Algo implements AM {
     public void run(AMInfo info) {
-        int a = info.parent.readInt();
-        int b = info.parent.readInt();
-        System.out.println(a + " - " + b + ". Build started.");
-
-        List<Integer> squares = new ArrayList<>();
-
-        IntStream.range(a, b).forEachOrdered(n -> {
-            IntStream.range(1, n / 2 + 1).forEachOrdered(k -> {
-                if (k * k == n) {
-                    squares.add(k);
-                }
-            });
-        });
-
-        System.out.println(a + " - " + b + ". Build finished.");
-        info.parent.write(squares.size());
+//        int a = info.parent.readInt();
+//        int b = info.parent.readInt();
+//        System.out.println(a + " - " + b + ". Build started.");
+//
+//        List<Integer> squares = new ArrayList<>();
+//
+//        IntStream.range(a, b).forEachOrdered(n -> {
+//            IntStream.range(1, n / 2 + 1).forEachOrdered(k -> {
+//                if (k * k == n) {
+//                    squares.add(k);
+//                }
+//            });
+//        });
+//
+//        System.out.println(a + " - " + b + ". Build finished.");
+        info.parent.write(1);
     }
 }
