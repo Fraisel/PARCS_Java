6164c61c78c745ddee6453d020361c0183b81f3b
Denys Kochetkov <deniskochetkov1@gmail.com>
2021-04-08 02:57:33 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,21 @@ import parcs.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         task curtask = new task();
-        curtask.addJarFile("Algo.jar");
+        curtask.addJarFile("Square.jar");
 
         AMInfo info = new AMInfo(curtask, null);
 
-//         Scanner sc = new Scanner(new File(curtask.findFile("input")));
-//         int a = sc.nextInt();
-//         int b = sc.nextInt();
-//         System.out.println(a);
-//         System.out.println(b);
-        
+        Scanner sc = new Scanner(new File(curtask.findFile("input")));
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+
         point p = info.createPoint();
         channel c = p.createChannel();
-        p.execute("Algo");
-//         c.write(a);
-//         c.write(b);
+        p.execute("Square");
+        c.write(a);
+        c.write(b);
 
         System.out.println("Waiting for result...");
         System.out.println("Result: " + c.readInt());
